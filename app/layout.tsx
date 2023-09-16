b1decf9476f3bb76fc17b28cbe171eb1db811cf6
mohammedkmo <alrasamking123@gmail.com>
2023-09-17 01:19:47 +0300

    support PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,43 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='dark'>
+      <head>
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
 
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-512x512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-512x512.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-512x512.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-48x48.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://js.mohammedk.me" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta name="twitter:image" content="https://js.mohammedk.me/icons/icon-192x192.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://js.mohammedk.me" />
+        <meta property="og:image" content="https://js.mohammedk.me/icons/icon-48x48.png" />
+      </head>
       <body className={spaceMono.className}>
 
         <div className='w-full border-b sticky top-0 z-50 bg-black'>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+    //...
+  })
+  
+  const nextConfig = {}
 
-module.exports = nextConfig
+  module.exports = withPWA({
+    nextConfig
+  })
+
+
